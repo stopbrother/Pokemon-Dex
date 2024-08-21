3923fdc7bbb5cfd815ed494d9bf76335a30f0c6e
stopbrother <stopbro0323@naver.com>
2024-08-21 16:41:47 +0900

    add : react router로 Home페이지와 Dex페이지 구성

---
+++ src/shared/Router.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "../pages/Home";
+import Dex from "../pages/Dex";
+
+function Router() {
+  return (
+    //Routes를 BrowserRouter로 감싼이유 : 브라우저가 깜빡이지 않고 페이지 이동하기 위해
+    <BrowserRouter>
+      <Routes>
+        {/* path는 사용하고 싶은 주소
+            element는 이동했을때 보여주고자 하는 컴포넌트 */}
+        <Route path="/" element={<Home />} />
+        <Route path="dex" element={<Dex />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default Router;
