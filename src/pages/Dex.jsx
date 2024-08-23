de540a304573d860679459c53222bd5efdd374a6
stopbrother <stopbro0323@naver.com>
2024-08-23 15:40:08 +0900

    add : 포켓몬 선택 유효성 검사 메시지 띄우기

--- src/pages/Dex.jsx
+++ src/pages/Dex.jsx
@@ -15,6 +15,16 @@ function Dex() {
   const [selectedPokemon, setSelectedPokemon] = useState([]);
 
   const addPokemon = (pokemon) => {
+    const selectedCheck = selectedPokemon.some(
+      (selected) => pokemon.id === selected.id
+    );
+
+    if (selectedPokemon.length >= 6) {
+      return alert("최대 6개의 포켓몬만 선택할 수 있습니다.");
+    }
+    if (selectedCheck) {
+      return alert("이미 선택한 포켓몬 입니다.");
+    }
     setSelectedPokemon((prevSelectedPokemon) => [
       ...prevSelectedPokemon,
       pokemon,
