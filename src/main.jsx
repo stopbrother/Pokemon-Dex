d7ac8b714e83877b32a42a3bc98e4c20281e78ac
stopbrother <stopbro0323@naver.com>
2024-08-27 19:39:45 +0900

    import 부분 삭제

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./css/index.css";
 import "./css/reset.css";
-import "./css/dex.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
